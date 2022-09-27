ab2a7355f3ae211f3ac8a19b25d0eadc6259a650
Noreen Arshads <noreen@Noreens-MBP.lan>
2022-09-26 23:30:19 -0700

    Create function to generate the markdown and fixed badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (value === "Mozilla") {
@@ -15,7 +15,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (value === "Mozilla") {
@@ -32,7 +32,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
@@ -49,10 +49,69 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   
   return `# ${data.title}
+  ${data.description}
+  ${data.licenseBadge}
+  [A deployed version can be viewed here.](${data.URL})
+  
+  ---
+  ## Contents
+  1. [About](#about)
+  2. [User Story](#user%20story)
+  3. [Visuals](#visuals)
+  4. [Installation](#installation)
+  5. [License](#license)
+  6. [Contributing](#contributing)
+  7. [Tests](#tests)
+  8. [Authors and acknowledgment](#authors%20and%20acknowledgment)
+  ---
+  ## About
+    ${data.about}
+  ---
+  ## User Story
+    
+  ---
+  ## Acceptance Criteria
+    
+    
+  ---
+  ## Visuals:
+    ![]()
+  ---
+  ## Installation:
+    ${data.installation}
+    To clone the repo:
+        git clone ${data.clone}
+    
+  ---
+  ## License
+    License used for this project - ${data.license}
+    * For more information on license types, please reference this website 
+      [https: //choosealicense.com/](https://choosealicense.com/).
+  ---
+  ## Contributing:
+    
+    To contribute to this application, create a pull request.
+    Here are the steps needed for doing that:
+    - Fork the repo
+    - Create a feature branch (git checkout -b NAME-HERE)
+    - Commit your new feature (git commit -m 'Add some feature')
+    - Push your branch (git push)
+    - Create a new Pull Request
+    Following a code review, your feature will be merged.
+  ---
+  ## Tests:
+    ${data.test}
+  ---
+  ## Authors and Acknowledgments
+    ${data.author}
+  ---
+  ## Contact Information:
+  * GitHub Username: ${data.userName}
+  * GitHub Email: ${data.userEmail}
 
 `;
 }
